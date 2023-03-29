e8514cccf402084195c857cd136902c140e1b345
eattato <80892781+eattato@users.noreply.github.com>
2023-03-30 00:46:53 +0900

    카드 누르면 이동되게 했음

--- public/main/js/script.js
+++ public/main/js/script.js
@@ -30,22 +30,26 @@ function eraseCookie(name) {
 $().ready(() => {
   let characters = $(".character_list");
 
-  const addCharacterCard = (name, face, element, star) => {
+  const addCharacterCard = (data) => {
     let card =
       '<div class="character_frame"><img class="character_element" src="" /><div class="character_image"><img src="" /></div><div class="character_name"></div></div>';
     card = $($.parseHTML(card));
-    card.find(".character_name").text(name);
-    card.find(".character_image img").attr("src", face);
+    card.find(".character_name").text(data.name);
+    card.find(".character_image img").attr("src", data.icon);
 
-    let elementPath = "../resource/images/elements/" + element + "_blur.png";
+    let elementPath =
+      "../resource/images/elements/" + data.element + "_blur.png";
     card.find(".character_element").attr("src", elementPath);
 
-    if (star == 5) {
+    if (data.rarity == 5) {
       card.addClass("five_star");
     } else {
       card.addClass("four_star");
     }
 
+    card.click(() => {
+      window.location.href = data.id;
+    });
     card.appendTo(characters);
   };
 
@@ -53,12 +57,7 @@ $().ready(() => {
     if (characters) {
       for (let ind in characters) {
         let avatar = characters[ind];
-        addCharacterCard(
-          avatar.name,
-          avatar.icon,
-          avatar.element,
-          avatar.rarity
-        );
+        addCharacterCard(avatar);
       }
     }
   };
